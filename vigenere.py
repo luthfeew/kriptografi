21265c3fc1488820b196f56d7fdccc1e53102ef5
luthfeew <luthfeew@gmail.com>
2022-06-08 03:51:10 +0700

    fungsi dasar & mode clear

--- vigenere.py
+++ vigenere.py
@@ -1,6 +1,48 @@
+from pyodide import create_proxy
+from js import console
+
 input = Element('input')
-output = Element('output')
+choice = Element('choice')
 kunci = Element('key')
+mode = Element('mode')
+output = Element('output')
+t_enkripsi = Element('tab_enkripsi')
+t_dekripsi = Element('tab_dekripsi')
+title_in = Element('title_in')
+title_out = Element('title_out')
+
+
+def switch_input():
+    a = input.element.value
+    b = output.element.value
+    input.element.value = b
+    output.element.value = a
+
+
+def tab_enkripsi_click(event):
+    t_enkripsi.element.classList.add('is-active')
+    t_dekripsi.element.classList.remove('is-active')
+    title_in.element.innerHTML = 'Plainteks'
+    title_out.element.innerHTML = 'Cipherteks'
+    choice.element.value = 0
+    switch_input()
+
+
+def tab_dekripsi_click(event):
+    t_dekripsi.element.classList.add('is-active')
+    t_enkripsi.element.classList.remove('is-active')
+    title_in.element.innerHTML = 'Cipherteks'
+    title_out.element.innerHTML = 'Plainteks'
+    choice.element.value = 1
+    switch_input()
+
+
+def main():
+    choice.element.value = 0
+    t_enkripsi.element.addEventListener(
+        'click', create_proxy(tab_enkripsi_click))
+    t_dekripsi.element.addEventListener(
+        'click', create_proxy(tab_dekripsi_click))
 
 
 def enkripsi(plaintext, num_key):
@@ -46,7 +88,7 @@ def dekripsi(ciphertext, num_key):
 
 
 def proses(*args, **kwargs):
-    plaintext = input.value
+    inputan = input.value
     key0 = kunci.value
 
     num_key = []
@@ -58,6 +100,25 @@ def proses(*args, **kwargs):
 
     # console.log(num_key)
     # console.log(key0)
+    # console.log(mode.value)
 
-    ciphertext = enkripsi(plaintext, num_key)
-    output.element.value = ciphertext
+    match int(choice.value):
+        case 0:
+            x = enkripsi(inputan, num_key)
+        case 1:
+            x = dekripsi(inputan, num_key)
+
+    match int(mode.value):
+        case 0:
+            x = x
+        case 1:
+            x = x.replace(' ', '')
+        case 2:
+            x = x.replace(' ', '')
+            x = [x[i:i + 5] for i in range(0, len(x), 5)]
+            x = ' '.join(x)
+
+    output.element.value = x
+
+
+main()
