751a102933337fe4acc9a04d55b36c4b4afa86f7
luthfeew <luthfeew@gmail.com>
2022-06-27 09:37:46 +0700

    Black formatter

--- vigenere.py
+++ vigenere.py
@@ -1,21 +1,21 @@
 import base64
 from pyodide import create_proxy
-from js import console, document, FileReader, window, encodeURIComponent
+from js import document, FileReader, window, encodeURIComponent
 
-input = Element('input')
-p_input = Element('p_input')
-file_upload = Element('upload')
-upload_name = Element('upload_name')
-choice = Element('choice')
-jenis = Element('jenis')
-kunci = Element('key')
-p_kunci = Element('p_key')
-mode = Element('mode')
-output = Element('output')
-title_in = Element('title_in')
-title_out = Element('title_out')
-t_enkripsi = Element('tab_enkripsi')
-t_dekripsi = Element('tab_dekripsi')
+input = Element("input")
+p_input = Element("p_input")
+file_upload = Element("upload")
+upload_name = Element("upload_name")
+choice = Element("choice")
+jenis = Element("jenis")
+kunci = Element("key")
+p_kunci = Element("p_key")
+mode = Element("mode")
+output = Element("output")
+title_in = Element("title_in")
+title_out = Element("title_out")
+t_enkripsi = Element("tab_enkripsi")
+t_dekripsi = Element("tab_dekripsi")
 
 
 def switch_input():
@@ -26,130 +26,145 @@ def switch_input():
 
 
 def tab_enkripsi_click(e):
-    if t_enkripsi.element.classList.contains('is-active'):
+    if t_enkripsi.element.classList.contains("is-active"):
         pass
     else:
-        t_enkripsi.element.classList.add('is-active')
-        t_dekripsi.element.classList.remove('is-active')
-        title_in.element.innerHTML = 'Plainteks'
-        title_out.element.innerHTML = 'Cipherteks'
+        t_enkripsi.element.classList.add("is-active")
+        t_dekripsi.element.classList.remove("is-active")
+        title_in.element.innerHTML = "Plainteks"
+        title_out.element.innerHTML = "Cipherteks"
         choice.element.value = 0
         switch_input()
 
 
 def tab_dekripsi_click(e):
-    if t_dekripsi.element.classList.contains('is-active'):
+    if t_dekripsi.element.classList.contains("is-active"):
         pass
     else:
-        t_dekripsi.element.classList.add('is-active')
-        t_enkripsi.element.classList.remove('is-active')
-        title_in.element.innerHTML = 'Cipherteks'
-        title_out.element.innerHTML = 'Plainteks'
+        t_dekripsi.element.classList.add("is-active")
+        t_enkripsi.element.classList.remove("is-active")
+        title_in.element.innerHTML = "Cipherteks"
+        title_out.element.innerHTML = "Plainteks"
         choice.element.value = 1
         switch_input()
 
 
 def input_change(e):
-    input.element.classList.remove('is-danger')
-    p_input.element.classList.remove('help', 'is-danger')
-    p_input.element.innerHTML = ''
+    input.element.classList.remove("is-danger")
+    p_input.element.classList.remove("help", "is-danger")
+    p_input.element.innerHTML = ""
 
 
 def kunci_change(e):
-    kunci.element.classList.remove('is-danger')
-    p_kunci.element.classList.remove('help', 'is-danger')
-    p_kunci.element.innerHTML = ''
-
-
-def read_complete(e):
-    input.element.value = e.target.result
+    kunci.element.classList.remove("is-danger")
+    p_kunci.element.classList.remove("help", "is-danger")
+    p_kunci.element.innerHTML = ""
 
 
 async def process_file(x):
-    fileList = document.getElementById('upload').files
+    fileList = document.getElementById("upload").files
 
     for f in fileList:
         reader = FileReader.new()
         reader.onloadend = read_complete
         reader.readAsText(f)
         upload_name.element.innerHTML = f.name
+        input_change(None)
+
+
+def read_complete(e):
+    input.element.value = e.target.result
+
+
+def unduh_file(*args, **kwargs):
+    link = window.document.createElement("a")
+    link.setAttribute(
+        "href",
+        "data:text/plain;charset=utf-8," + encodeURIComponent(output.element.value),
+    )
+    link.setAttribute("download", "output.txt")
+    link.click()
 
 
 def enkripsi(plaintext, num_key):
     count = 0
-    ciphertext = ''
+    ciphertext = ""
+
     for i in range(len(plaintext)):
         char0 = plaintext[i]
+
         match int(jenis.value):
             case 1:
                 if count < len(num_key):
                     key1 = num_key[count]
-                    ciphertext += bytes([(ord(char0) + key1) %
-                                        256]).decode('cp437')
+                    ciphertext += bytes([(ord(char0) + key1) % 256]).decode("cp437")
                     count += 1
                 if count == len(num_key):
                     count = 0
             case _:
                 char = char0.lower()
                 if char == " ":
-                    ciphertext += ' '
+                    ciphertext += " "
                 elif char.isdigit():
                     ciphertext += char
                 elif char.isalpha():
                     if count < len(num_key):
                         key1 = num_key[count]
+
                         match int(jenis.value):
                             case 0:
-                                ciphertext += chr((ord(char) +
-                                                  key1 - 97) % 26 + 97)
+                                ciphertext += chr((ord(char) + key1 - 97) % 26 + 97)
                             case 2:
-                                ciphertext += chr((key1 -
-                                                  ord(char) + 97 % 26) % 26 + 97)
+                                ciphertext += chr(
+                                    (key1 - ord(char) + 97 % 26) % 26 + 97
+                                )
+
                         count += 1
                     if count == len(num_key):
                         count = 0
 
     if int(jenis.value) == 1:
-        ciphertext = base64.b64encode(
-            ciphertext.encode('utf-8')).decode('utf-8')
+        ciphertext = base64.b64encode(ciphertext.encode("utf-8")).decode("utf-8")
 
     return ciphertext
 
 
 def dekripsi(ciphertext, num_key):
     count = 0
-    plaintext = ''
+    plaintext = ""
 
     if int(jenis.value) == 1:
-        ciphertext = base64.b64decode(ciphertext).decode('utf-8')
+        ciphertext = base64.b64decode(ciphertext).decode("utf-8")
 
     for i in range(len(ciphertext)):
         char0 = ciphertext[i]
+
         match int(jenis.value):
             case 1:
                 if count < len(num_key):
                     key1 = num_key[count]
-                    plaintext += bytes([(ord(char0.encode('cp437')) - key1) %
-                                       256]).decode('cp437')
+                    plaintext += bytes(
+                        [(ord(char0.encode("cp437")) - key1) % 256]
+                    ).decode("cp437")
                     count += 1
                 if count == len(num_key):
                     count = 0
             case _:
                 char = char0.lower()
                 if char == " ":
-                    plaintext += ' '
+                    plaintext += " "
                 elif char.isdigit():
                     plaintext += char
                 elif char.isalpha():
                     if count < len(num_key):
                         key1 = num_key[count]
+
                         match int(jenis.value):
                             case 0:
-                                plaintext += chr((ord(char) -
-                                                 key1 - 97) % 26 + 97)
+                                plaintext += chr((ord(char) - key1 - 97) % 26 + 97)
                             case 2:
-                                plaintext += chr((key1 -
-                                                  ord(char) + 97 % 26) % 26 + 97)
+                                plaintext += chr((key1 - ord(char) + 97 % 26) % 26 + 97)
+
                         count += 1
                     if count == len(num_key):
                         count = 0
@@ -163,17 +178,18 @@ def proses(*args, **kwargs):
 
     if not inputan or not key0:
         if not inputan:
-            input.element.classList.add('is-danger')
-            p_input.element.classList.add('help', 'is-danger')
-            p_input.element.innerHTML = 'Input tidak boleh kosong'
+            input.element.classList.add("is-danger")
+            p_input.element.classList.add("help", "is-danger")
+            p_input.element.innerHTML = "Input tidak boleh kosong"
         if not key0:
-            kunci.element.classList.add('is-danger')
-            p_kunci.element.classList.add('help', 'is-danger')
-            p_kunci.element.innerHTML = 'Kunci tidak boleh kosong'
+            kunci.element.classList.add("is-danger")
+            p_kunci.element.classList.add("help", "is-danger")
+            p_kunci.element.innerHTML = "Kunci tidak boleh kosong"
         return
 
     num_key = []
     key = key0.lower()
+
     for i in range(len(key)):
         key1 = key[i]
         num_key.append(ord(key1) - 97)
@@ -188,33 +204,22 @@ def proses(*args, **kwargs):
         case 0:
             x = x
         case 1:
-            x = x.replace(' ', '')
+            x = x.replace(" ", "")
         case 2:
-            x = x.replace(' ', '')
-            x = [x[i:i + 5] for i in range(0, len(x), 5)]
-            x = ' '.join(x)
+            x = x.replace(" ", "")
+            x = [x[i : i + 5] for i in range(0, len(x), 5)]
+            x = " ".join(x)
 
     output.element.value = x
 
 
-def unduh_file(*args, **kwargs):
-    link = window.document.createElement('a')
-    link.setAttribute('href', 'data:text/plain;charset=utf-8,' +
-                      encodeURIComponent(output.element.value))
-    link.setAttribute('download', 'output.txt')
-    link.click()
-
-
 def main():
     choice.element.value = 0
-    t_enkripsi.element.addEventListener(
-        'click', create_proxy(tab_enkripsi_click))
-    t_dekripsi.element.addEventListener(
-        'click', create_proxy(tab_dekripsi_click))
-    file_upload.element.addEventListener(
-        'change', create_proxy(process_file))
-    input.element.addEventListener('input', create_proxy(input_change))
-    kunci.element.addEventListener('input', create_proxy(kunci_change))
+    t_enkripsi.element.addEventListener("click", create_proxy(tab_enkripsi_click))
+    t_dekripsi.element.addEventListener("click", create_proxy(tab_dekripsi_click))
+    file_upload.element.addEventListener("change", create_proxy(process_file))
+    input.element.addEventListener("input", create_proxy(input_change))
+    kunci.element.addEventListener("input", create_proxy(kunci_change))
 
 
 main()
